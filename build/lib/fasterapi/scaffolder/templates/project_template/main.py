bcda7aa8caa7e99abefd1e31b8deba1442d29450
nathanieluriri <uririnathaniel@gmail.com>
2025-07-28 13:11:45 +0100

    New Release ≡ƒÑç

--- build/lib/fasterapi/scaffolder/templates/project_template/main.py
+++ build/lib/fasterapi/scaffolder/templates/project_template/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from schemas.response_schema import APIResponse
 
 app = FastAPI()
 
@@ -11,7 +13,16 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
 )
-
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=APIResponse(
+            status_code=exc.status_code,
+            data=None,
+            detail=exc.detail,
+        ).dict()
+    )
 @app.get("/")
 def read_root():
     return {"message": "Hello from FasterAPI!"}
